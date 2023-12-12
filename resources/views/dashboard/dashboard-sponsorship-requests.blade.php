14770788f821106b60d601b86dd5cc91bb4c2bb5
website-f <mohdfitri1017@gmail.com>
2023-12-12 18:02:10 +0800

    add

--- resources/views/dashboard/dashboard-sponsorship-requests.blade.php
+++ resources/views/dashboard/dashboard-sponsorship-requests.blade.php
@@ -37,6 +37,20 @@
       
       <div class="row">
 
+        <div class="col-lg-12">
+          <div class="callout callout-info">
+            <h5>There's Sponsorship Request waiting for your approval</h5>
+
+            <p>
+              @if ($sponsor->status == "proof")
+              <a href="#proofAttachments" class="btn btn-info">View Proof Of Agreements</button>
+              @else
+
+              @endif
+            </p>
+          </div>
+        </div>
+
         <div class="col-12">
           <div class="card">
             <div class="card-header">
@@ -67,7 +81,7 @@
           </div>
         </div>
 
-        <div class="col-8">
+        <div class="col-7">
           <div class="card">
             <div class="card-header">
               <h3 class="card-title">
@@ -75,16 +89,126 @@
               </h3>
             </div>
             <div class="card-body">
-              
+              <div class="row">
+                <div class="col-lg-6">
+                  <label class="form-label">Name of the event / Project</label>
+                  <input type="text" class="form-control" readonly value="{{$sponsor->event_name}}"><br>
+                </div>
+                <div class="col-lg-6">
+                  <label class="form-label">Nature of the Event</label>
+                  <input type="text" class="form-control" readonly value="{{$sponsor->nature_event}}"><br>
+                </div>
+                <div class="col-lg-6">
+                  <label class="form-label">From</label>
+                  <input type="text" class="form-control" readonly value="{{$sponsor->from_date}}"><br>
+                </div>
+                <div class="col-lg-6">
+                  <label class="form-label">To</label>
+                  <input type="text" class="form-control" readonly value="{{$sponsor->to_date}}"><br>
+                </div>
+                <div class="col-lg-12">
+                  <label class="form-label">Event Address</label>
+                  <input type="text" class="form-control" readonly value="{{$sponsor->eventAddress}}"><br>
+                </div>
+                <div class="col-lg-12">
+                  <label class="form-label">Explanation of Product Use</label>
+                  <input type="text" class="form-control" readonly value="{{$sponsor->explaination_product}}"><br>
+                </div>
+                <div class="col-lg-6">
+                  <label class="form-label">Number of Expected Attendees</label>
+                  <input type="text" class="form-control" readonly value="{{$sponsor->attendees}}"><br>
+                </div>
+                <div class="col-lg-6">
+                  <label class="form-label">Can we place booth in your event?</label>
+                  <input type="text" class="form-control" readonly value="{{$sponsor->booth}}"><br>
+                </div>
+                <div class="col-lg-12 mb-3">
+                  <label class="form-label">Sponsorhip Attachments</label><br>
+                  @php
+                    $attach = json_decode($sponsor->sposorship_attachments);
+  
+                    if ($attach !== null) {
+                      $attachCounts = count($attach);
+                    } else {
+                      $attachCounts = 0;
+                    }
+                  @endphp
+                  <!-- Button trigger modal -->
+                  <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModalCenter">
+                    View all attachments ({{$attachCounts}})
+                  </button><br>
+                  
+                  <!-- Modal -->
+                  <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+                    <div class="modal-dialog modal-dialog-centered" role="document">
+                      <div class="modal-content">
+                        <div class="modal-header">
+                          <h5 class="modal-title" id="exampleModalLongTitle"><a href="#" class="btn btn-info">Download All</a></h5>
+                          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                          </button>
+                        </div>
+                        <div class="modal-body">
+                          <div class="row">
+                            @if ($attach !== null)
+                                @foreach ($attach as $item)
+                                   @if (pathinfo($item, PATHINFO_EXTENSION) === 'jpg' || pathinfo($item, PATHINFO_EXTENSION) === 'jpeg' || pathinfo($item, PATHINFO_EXTENSION) === 'png' || pathinfo($item, PATHINFO_EXTENSION) === 'gif')     
+                                   {{-- <a data-glightbox data-gallery="gallery" href="{{ asset($item) }}">
+                                        <img class="img-fluid" src="{{ asset($item) }}" alt="Image" width="200px" height="200px"> 
+                                    </a>                         --}}
+                                    <div class="col-lg-4">
+                                      <a href="{{asset($item)}}">
+                                        <img class="img-fluid" src="{{asset($item)}}" alt="Image" width="150px" height="150px">
+                                      </a>
+                                    </div>
+                                   @else
+                                   <div class="col-lg-4">
+                                      <a class="btn btn-primary" href="{{ asset($item) }}" target="_blank">View File ({{pathinfo($item, PATHINFO_EXTENSION)}})</a>
+                                   </div>
+                                   @endif
+                                @endforeach
+                            @endif
+                          </div>
+                        </div>
+                        <div class="modal-footer">
+                          <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                          {{-- <button type="button" class="btn btn-primary">Save changes</button> --}}
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div> 
+
+                <div class="col-lg-6">
+                  <label class="form-label">Water - 200ml (48 Cups/carton)</label>
+                  <input type="text" class="form-control" readonly value="{{$sponsor->ro_200ml}} Cartons"><br>
+                </div>
+
+                <div class="col-lg-6">
+                  <label class="form-label">RO Water - 500ml (24 bottles/carton)</label>
+                  <input type="text" class="form-control" readonly value="{{$sponsor->ro_500ml}} Cartons"><br>
+                </div>
+
+                <div class="col-lg-6">
+                  <label class="form-label">RO Water - 11L (1 bottles)</label>
+                  <input type="text" class="form-control" readonly value="{{$sponsor->ro_11L}} Cartons"><br>
+                </div>
+
+                <div class="col-lg-6">
+                  <label class="form-label">Mineral Water - 350ml (24 bottles/carton)</label>
+                  <input type="text" class="form-control" readonly value="{{$sponsor->ro_350ml}} Cartons"><br>
+                </div>
+
+              </div>
             </div>
           </div>
         </div>
 
-        <div class="col-4">
+        <div class="col-5">
           <div class="card">
             <div class="card-header">
               <h3 class="card-title">
-                <b>Event / Project Details</b>
+                <b>Approval</b>
               </h3>
             </div>
             <div class="card-body">
