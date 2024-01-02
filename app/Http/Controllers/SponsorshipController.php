39071e7a23fdbb4c53324a74e6f2cc9e9a541d31
website-f <mohdfitri1017@gmail.com>
2024-01-02 16:18:06 +0800

    add

--- app/Http/Controllers/SponsorshipController.php
+++ app/Http/Controllers/SponsorshipController.php
@@ -25,37 +25,39 @@ class SponsorshipController extends Controller
             $fileDataJson = $request->input('file_names');
             $fileData = json_decode($fileDataJson);
             $files = [];
-            foreach ($fileData as $fileInfo) {
-                // Extract information from the file data
-                $name = $fileInfo->name;
-                $base64Data = $fileInfo->data;
-                $extension = $fileInfo->extension;
-        
-                // Generate a unique filename based on the current date and time
-                $currentDate = now(); // Use Laravel's now() helper function
-                $timestamp = $currentDate->timestamp; // Unique timestamp
-                $fileName = $name . $timestamp . '.' . $extension;
-        
-                // Decode the base64 data and save it with the unique filename
-                // $decodedData = base64_decode($base64Data);
-                // $destinationPath = public_path('uploads');
-                // $path = $destinationPath . '/' . $fileName;
-                if ($extension == "jpeg" || $extension == "jpg" || $extension == "png" || $extension == "gif") {
-                    $image = Image::make($base64Data);
-                    $image->encode($extension, 10);
-                    $destinationPath = public_path('uploads');
-                    $path = $destinationPath . '/' . $fileName;
-                    $image->save($path);
+            if ($fileData !== null) {
+                foreach ($fileData as $fileInfo) {
+                    // Extract information from the file data
+                    $name = $fileInfo->name;
+                    $base64Data = $fileInfo->data;
+                    $extension = $fileInfo->extension;
+            
+                    // Generate a unique filename based on the current date and time
+                    $currentDate = now(); // Use Laravel's now() helper function
+                    $timestamp = $currentDate->timestamp; // Unique timestamp
+                    $fileName = $name . $timestamp . '.' . $extension;
+            
+                    // Decode the base64 data and save it with the unique filename
+                    // $decodedData = base64_decode($base64Data);
+                    // $destinationPath = public_path('uploads');
+                    // $path = $destinationPath . '/' . $fileName;
+                    if ($extension == "jpeg" || $extension == "jpg" || $extension == "png" || $extension == "gif") {
+                        $image = Image::make($base64Data);
+                        $image->encode($extension, 10);
+                        $destinationPath = public_path('uploads');
+                        $path = $destinationPath . '/' . $fileName;
+                        $image->save($path);
+                    }
+                    else {
+                        $decodedData = base64_decode($base64Data);
+                        $destinationPath = public_path('uploads');
+                        $path = $destinationPath . '/' . $fileName;
+                        file_put_contents($path, $decodedData);
+                    }
+                    $files[] = "uploads/" . $fileName;
+            
+                    // Optionally, you can save the $uniqueFileName to your database for future reference
                 }
-                else {
-                    $decodedData = base64_decode($base64Data);
-                    $destinationPath = public_path('uploads');
-                    $path = $destinationPath . '/' . $fileName;
-                    file_put_contents($path, $decodedData);
-                }
-                $files[] = "uploads/" . $fileName;
-        
-                // Optionally, you can save the $uniqueFileName to your database for future reference
             }
         
             Mail::to('thezhencreative@gmail.com')->send(new SubmitNotification());
