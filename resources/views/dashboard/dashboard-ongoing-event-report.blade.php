ee2b8db843053d8e3f119ff83f34752501adeedb
website-f <mohdfitri1017@gmail.com>
2024-01-26 15:57:38 +0800

    add

--- resources/views/dashboard/dashboard-ongoing-event-report.blade.php
+++ resources/views/dashboard/dashboard-ongoing-event-report.blade.php
@@ -35,7 +35,7 @@
               <!-- /.card-header -->
               <div class="card-body">
                 <table id="example1" class="table table-bordered table-striped">
-                  <thead>
+                  <thead class="bg-dark">
                   <tr>
                     <th>#</th>
                     <th>Event Name</th>
@@ -105,5 +105,12 @@
 
 
 <script src="{{asset('assets/plugins/jquery/jquery.min.js')}}"></script>
+<script>
+  $(function () {
+    $("#example1").DataTable({
+      "responsive": true, "lengthChange": false, "autoWidth": false,
 
+    }).buttons().container().appendTo('#example1_wrapper .col-md-6:eq(0)');
+  });
+</script>
 @endsection
